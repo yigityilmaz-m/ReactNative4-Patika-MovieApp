a8a75b968f98cbd80f3490de0358cfa0d53eeed4
yigityilmaz-m <yigityilmaz.m@gmail.com>
2021-12-15 23:36:58 +0300

    DesingElementsUpdated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Button, FlatList, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Pressable,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  Button,
+} from 'react-native';
 import styles from './Home.styles';
 import MovieCard from '../../components/MovieItem/MovieCard';
 import axios from 'axios';
 import {useNavigation, useRoute} from '@react-navigation/core';
 import {SafeAreaView} from 'react-native-safe-area-context';
+
 
 const baseURL = 'http:10.0.3.2:3000';
 
@@ -19,38 +28,74 @@ function Home() {
   }
 
   const renderItem = ({item}) => (
-    
-      <View>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('MoviePage', item)}>
-          <MovieCard
-            style={styles.containerAlt}
-            name={item.name}
-            rate={item.rate}
-            genre={item.genre}
-            brief={item.brief.slice(0, 200) + '...'}
-          />
-        </TouchableOpacity>
-      </View>
-    
+    <View>
+      <TouchableOpacity onPress={() => navigation.navigate('MoviePage', item)}>
+        <MovieCard
+          style={styles.containerAlt}
+          name={item.name}
+          rate={item.rate}
+          genre={item.genre}
+          brief={item.brief.slice(0, 200) + '...'}
+        />
+      </TouchableOpacity>
+    </View>
   );
 
   useEffect(() => {
     fetchData();
   }, []);
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>MOVIES</Text>
-      <View>
+      <View style={{flex:1}}>
+        <Text style={styles.header}>MOVIES</Text>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Genres</Text>
+                <Button style ={{margin:5}} color={'#c5cbe3'} title='action'/>
+                <Button color={'#c5cbe3'}  title='horror'/>
+                <Button color={'#c5cbe3'} title='thriller'/>
+                <Button color={'#c5cbe3'} title='drama'/>
+                <Button color={'#c5cbe3'} title='comedy'/>
+                <Button color={'#c5cbe3'}  title='romance'/>
+                <Button color={'#c5cbe3'}  title='fantasy'/>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => setModalVisible(true)}>
+            <Text style={styles.textStyle}>Filter by Genre</Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={{flex:7}}>
         <FlatList
           data={movies}
           renderItem={renderItem}
           keyExtractor={item => item.id}
         />
       </View>
+
     </SafeAreaView>
   );
 }
+
 
 export default Home;
--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -5,5 +5,56 @@ export default StyleSheet.create({
     backgroundColor: '#EFE2BA',
     flex: 1,
   },
-  header: {color:'#404040' ,fontSize: 35, fontWeight: 'bold',textAlign: 'center'},
+  header: {
+    color: '#404040',
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  centeredView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 5,
+    marginVertical: 5,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#c5cbe3',
+  },
+  buttonClose: {
+    backgroundColor: '#c5cbe3',
+  },
+  textStyle: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  btn: {
+    borderRadius: 20,
+    padding: 5,
+    marginBottom: 5,
+    elevation: 2,
+  },
 });
